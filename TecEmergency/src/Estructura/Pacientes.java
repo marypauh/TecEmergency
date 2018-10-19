4979fc5de359b792c3fd96ef4eda098be73e075d
Castillo666 <kevcastilloh@gmail.com>
2018-10-19 15:58:40 -0600

    Como les va

--- TecEmergency/src/Estructura/Pacientes.java
+++ TecEmergency/src/Estructura/Pacientes.java
@@ -1,17 +1,33 @@
 package Estructura;
 
 public class Pacientes {
-    String prioridad;
+    int prioridad;
     String ficha;
+    String color;
+    String padecimiento;
     
-    public Pacientes(String prioridad,String ficha){
-        this.prioridad = prioridad;
+    public Pacientes(int prioridad,String ficha,String color, String padecimiento){
+        this.prioridad = 0;
         this.ficha = ficha;
+        this.color = color;
+        this.padecimiento = padecimiento;
     }
-    public String getPrioridad() {
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getPadecimiento() {
+        return padecimiento;
+    }
+    public void setPadecimiento(String padecimiento) {
+        this.padecimiento = padecimiento;
+    }
+    public int getPrioridad() {
         return prioridad;
     }
-    public void setPrioridad(String prioridad) {
+    public void setPrioridad(int prioridad) {
         this.prioridad = prioridad;
     }
     public String getFicha() {
@@ -22,8 +38,7 @@ public class Pacientes {
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         return "Padecimiento:  " + prioridad+ " Ficha:  "+ ficha + "\t" ;
-    }
-    
+    }   
 }
